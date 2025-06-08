b287c1fa8f27c52d6dd5983d422efe26e3d5126d
Ilyas Fauzal Akbar <66202601+ilyasbit@users.noreply.github.com>
2025-06-09 01:36:38 +0700

    Update install-only.sh

--- install-only.sh
+++ install-only.sh
@@ -6,3 +6,4 @@ apt install -y tar screen wget curl git unzip --no-install-recommends
 wget https://github.com/ilyasbit/ss-easy-setup/raw/main/ss.tar.gz -O /tmp/ss.tar.gz
 tar -xvf /tmp/ss.tar.gz -C /tmp/
 mv /tmp/ss/* /usr/local/bin/
+mkdir -p /etc/shadowsocks/
